2a57abc8cf63edff029e150313da8dfee95acdcd
kavitaparsewar <parsewarkavita@gmail.com>
2021-10-24 18:14:20 +0530

    [kavita] added UC1

---
+++ UC1_tictactoegame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workshop1_tiktactoe
+{
+    public class UC1_tictactoegame
+    {
+
+        UC1_tictactoegame obj = new UC1_tictactoegame();
+
+
+        public static void tictactoe()
+        {
+            //declaring array
+            char[] board = new char[10];
+
+            for (int i = 1; i < 10; i++)
+            {
+                //assigning empty value
+                board[i] = ' ';
+            }
+        }
+    }
+}
