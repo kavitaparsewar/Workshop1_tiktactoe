7843feb2f070ab0914fafc4642e9af7b63915ac4
kavitaparsewar <parsewarkavita@gmail.com>
2021-10-24 18:19:43 +0530

    [kavita] added main program

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Workshop1_tiktactoe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to tic tac toe Game");
+
+
+
+            //#region UC1
+            //UC1_tictactoegame.tictactoe();
+            //#endregion UC1
+
+
+
+            //#region UC2
+            //UC2.tictactoe();
+            //UC2.chooseletter();
+            //#endregion UC2
+
+
+            #region UC3 
+            UC3.showboard();
+            UC3.chooseletter();
+            #endregion UC3
+
+
+        }
+    }
+}
